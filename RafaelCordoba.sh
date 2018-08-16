1e4c7426c5133f95d707d36e14d6834ba95f7d74
rafaf991 <rf.cordoba@uniandes.edu.co>
2018-08-15 18:40:26 -0700

    Taller 1 lab moderna bien

--- RafaelCordoba.sh
+++ RafaelCordoba.sh
@@ -1,17 +1,15 @@
 wget https://raw.githubusercontent.com/daniel-lozano/CLASES_PYTHON/master/CLASE1/HERRAMIENTAS/notas.txt
-touch contador.txt
-touch contador2.txt
+
 touch RES1.txt
 
-awk '{if($4 >4) print 2 }' notas.txt > contador.txt
-wc contador.txt> contador2.txt
-awk '{print "hay",$1,"estudiantes con nota superior a 4" }' contador2.txt > RES1.txt
-rm -rf contador.txt
-rm -rf contador2.txt
-touch contador.txt
-touch contador2.txt
-awk '{if($4 > 4) print $0}' notas.txt | awk '{if($6 > 15) print $0}' notas.txt > contador.txt
+awk '{if($3 > 4) print $0}' notas.txt| wc |awk '{print "hay ",$1-1, "estudiantes con nota superior a 4"}' > RES1.txt
+awk '{if($3 > 4) print $0}' notas.txt|awk '{if ($6 > 15) print $1-1}' |wc |awk '{print "entre ellos,"$1, "tuvieron mas de 15 en el final"}' >> RES1.txt
+echo "Los estudiantes con un cero y que perdieron la materia son:"
+grep -w "0" notas.txt | awk '{if ($7<6) print $1 ,$2}'
 
-wc contador.txt > contador2.txt
-awk '{print "hay",$1,"estudiantes con nota superior a 15 en el final" }' contador2.txt >>RES1.txt 
+touch RES2.txt
+echo "Los estudiantes con nota final mayor a 8 son:" > RES2.txt
 
+awk '{if($7> 8 && $7 != "Puntos" && $7 != "Laboratorios") print $1,$2}' notas.txt >> RES2.txt
+
+cat RES2.txt
