a5ea17fd42707934b185b4ea8b53d46ff8ead8b2
rafael <rf.cordoba@uniandes.edu.co>
2018-10-17 21:09:56 -0700

    Tarea 3 Transformada fourier implementacion propia(1.3)

--- metodos_computacionales/Tarea 3/Fourier.py
+++ metodos_computacionales/Tarea 3/Fourier.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.fft as fft
 
 
 signal=np.genfromtxt("signal.dat",delimiter =",")
@@ -9,3 +10,23 @@ incompletos=np.genfromtxt("incompletos.dat",delimiter =",")
 plt.plot(signal[:,0],signal[:,1],label= "Datos signal")
 plt.legend(loc=0)
 plt.savefig("CordobaRafael_signal.pdf")
+
+
+def fft1(x):
+
+    N=len(x)
+    X=[]
+    for k in range(0,N):
+        sum=0
+        for n in range (0,N):
+            sum+=x[n]*np.exp(-1j*2*np.pi*(k*n)/N)
+        X.append(sum)
+    return X
+
+X=fft1(signal[:,1])
+t=np.linspace(0,len(X),len(X))
+plt.clf()
+plt.plot(t,np.abs(X),label="Transformada implementacion propia")
+#plt.plot(t,fft.fft(signal[:,1]),label="Transformada implementacion np.")
+plt.legend(loc=0)
+#plt.show()
