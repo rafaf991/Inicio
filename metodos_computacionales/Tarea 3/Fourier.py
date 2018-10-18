ea89d2f779e0915647f79a4b33a29a5d64866a7e
rafael <rf.cordoba@uniandes.edu.co>
2018-10-17 20:51:00 -0700

    Tarea 3 punto 1.2

--- metodos_computacionales/Tarea 3/Fourier.py
+++ metodos_computacionales/Tarea 3/Fourier.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 
 signal=np.genfromtxt("signal.dat",delimiter =",")
 incompletos=np.genfromtxt("incompletos.dat",delimiter =",")
-print signal
-print incompletos
+
+
+plt.plot(signal[:,0],signal[:,1],label= "Datos signal")
+plt.legend(loc=0)
+plt.savefig("CordobaRafael_signal.pdf")
